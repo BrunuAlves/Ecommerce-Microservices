aaab8ac8ecc08c9ea611f6ce1698fde6cf7138dc
Bruno Alves <149032011+BrunuAlves@users.noreply.github.com>
2025-10-27 16:42:54 -0300

    Add data annotations to Product model
    Added data annotations for entity framework.

--- StockService/Models/Product.cs
+++ StockService/Models/Product.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace StockService.Models
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+        
         public int Quantity { get; set; }
         public int SellerId { get; set; }
     }
